e4fc277cd5378ed35f63e168cfb919dd2c1f4c0d
LuisArroyo17 <luis.arroyo3@unmsm.edu.pe>
2024-11-12 14:14:12 -0500

    Revert "."
    This reverts commit 0de3d765b3ef4166123a2b53acbc0fc76a5e1a72.

--- static/uploads/scipt1.py
+++
@@ -1,5 +0,0 @@
-def solve(n):
-    suma = 0
-    for i in range(n):
-        suma += i
-    return suma
--- static/uploads/script2.py
+++
@@ -1,4 +0,0 @@
-def solve(n):
-    return n * (n + 1) // 2
-n = 10
-print(f"La suma de los números del 1 al {n} es: {solve(n)}")
